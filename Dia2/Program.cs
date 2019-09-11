09fe7ceb3d823be23ad40cd69d1dfb695eddb972
Deivis Costa Pereira <20757773+oscaruno@users.noreply.github.com>
2019-09-11 12:54:48 -0300

    Add files via upload

--- Dia2/Program.cs
+++ Dia2/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using Entidades;
 using DAL;
@@ -11,23 +10,32 @@ namespace aplicacao
 {
     class Program{
         static void Main(string[] args){
-            /*using (var reader = new StreamReader(@"C:\Users\Administrador\Desktop\min-csharp-master\min-csharp-master\dia2\cidade.csv")){
-                while (!reader.EndOfStream){
-                    var line = reader.ReadLine();
-                    var values = line.Split(',');
-
-                    Cidade cid = new Cidade();
-                    cid.CID_NOME = values[0];
-                    cid.CID_UF = values[1];
-                    CidadeDAL dal = new CidadeDAL();
-                    dal.Insert(cid);
-                }
-            }*/
             CidadeDAL dal = new CidadeDAL();
+            //using (var reader = new StreamReader(@"CAMINHO\cidade.csv")){
+            //    while (!reader.EndOfStream){
+            //        var line = reader.ReadLine();
+            //        var values = line.Split(',');
+            //        Cidade cid = new Cidade
+            //        {
+            //            CID_NOME = values[0],
+            //            CID_UF = values[1]
+            //        };
+            //        dal.Insert(cid);
+            //    }
+            //}
             List<Cidade> lscid = dal.Select();
-            lscid.Where(x => x.CID_UF == "RS").ToList().ForEach(x => {
-                Console.WriteLine(x.CID_NOME);
-            });
+
+            foreach(var t in lscid){
+                Console.WriteLine(t.CID_NOME);
+            }
+
+            //IEnumerable<Cidade> sel = from cid in lscid
+            //                   where cid.CID_UF.Equals("RS")
+            //                   select cid;
+            //lscid.Where(x => x.CID_UF == "RS").ToList().ForEach(x => {
+            //    Console.WriteLine(x.CID_NOME);
+            //});
+
             Console.ReadKey();
         }
     }
